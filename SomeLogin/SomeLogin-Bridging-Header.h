afaf2d5a88473ea88b569f978203b4812124592f
Ing. Enrique Ugalde <eugalde@ing-enrique-ugalde-macbook-pro.local>
2016-07-24 20:35:38 -0500

    Added Google Sign in Method

---
+++ SomeLogin/SomeLogin-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SomeLogin-Bridging-Header.h
+//  SomeLogin
+//
+//  Created by Ing. Enrique Ugalde on 24/07/16.
+//  Copyright © 2016 Ing. Enrique Ugalde. All rights reserved.
+//
+
+#ifndef SomeLogin_Bridging_Header_h
+#define SomeLogin_Bridging_Header_h
+
+#import <GoogleSignIn/GoogleSignIn.h>
+
+
+#endif /* SomeLogin_Bridging_Header_h */
--- SomeLogin/ViewController.swift
+++ SomeLogin/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
 
-class ViewController: UIViewController {
+//Google Sign in add GIDSignInDelegate, GIDSignInUIDelegate
+//Para hacer el boton de google sign in es necesario una view y añadirle la clase GIDSignInButton medidas 125x45
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // for google sign in
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func bt(sender: AnyObject) {
+    }
+    
+    
+    //for google sign in
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError!) {
+        if let error = error{
+            print(error.localizedDescription)
+            return
+        }
+        let authentication = user.authentication
+        let credential = FIRGoogleAuthProvider.credentialWithIDToken(authentication.idToken, accessToken: authentication.accessToken)
+        FIRAuth.auth()?.signInWithCredential(credential, completion: {
+            (user, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+                return
+            }
+            print("acceso con google")
+        })
+    }
 
+    //for google sign in
+    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user: GIDGoogleUser!, withError error: NSError!) {
+        if let error = error{
+            print(error.localizedDescription)
+            return
+        }
+        try! FIRAuth.auth()!.signOut()
+    }
 }
 
